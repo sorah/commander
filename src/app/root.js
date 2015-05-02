418ba874171e271189e0b656768588b694dec327
Shota Fukumori (sora_h) <her@sorah.jp>
2015-05-03 01:45:35 +0900

    remove useless logging

--- src/app/root.js
+++ src/app/root.js
@@ -4,7 +4,7 @@
   var remote = require('remote');
   if (remote.getGlobal('reactDevMode') == true) {
     var BrowserWindow = remote.require('browser-window');
-    console.log(BrowserWindow.addDevToolsExtension('./vendor/react-devtools'));
+    BrowserWindow.addDevToolsExtension('./vendor/react-devtools');
   }
 })()
 
